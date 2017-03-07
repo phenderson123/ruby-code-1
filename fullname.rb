69eeaaacbe6a334fcb5b204e694df59f9a684387
Perry E. Henderson <hendersonperry96@yahoo.com>
2017-03-07 18:21:12 -0500

    first commit

---
+++ fullname.rb
@@ -0,0 +1,8 @@
+puts "What's your first name?"
+f_name = gets.chomp
+puts "what's your middle name ?"
+m_name = gets.chomp
+puts "What's your last name"
+l_name = gets.chomp
+
+puts "That's a cool name !, #{f_name} #{m_name} #{l_name}."
